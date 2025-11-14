bf605a981d1b40e47c8f473162de2a7c3cff1680
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-11-14 03:04:40 -0300

    feat: health check endpoint

---
+++ ManageMe.Api/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using ManageMe.Api.Controllers.DTOs.Output;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ManageMe.Api.Controllers;
+
+[Authorize]
+public class HealthController : ManageMeController
+{
+    [HttpGet]
+    public ActionResult CollectData()
+    {
+        var process = Process.GetCurrentProcess();
+
+        var metrics = new
+        {
+            Status = "Healthy",
+            Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+        };
+
+        return Ok(BaseApiResponse.WithData(metrics.Status, new Dictionary<string, dynamic>()
+        {
+            { "status", metrics }
+        }));
+    }
+}
