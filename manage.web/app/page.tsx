c82f110b518dc805e2ec88755be4cec851e480ee
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-11-23 11:57:36 -0300

    wip: create register page and add link to it

--- manage.web/app/page.tsx
+++ manage.web/app/page.tsx
@@ -1,7 +1,9 @@
 import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
 
 export default function Home() {
-  return <main className="flex flex-col items-center justify-center min-h-full w-full">
+  return <main className="flex flex-col items-center justify-center min-h-full w-full gap-y-4">
     <LoginForm />
+    <Link className="text-blue-600" href={"/register"}>Create your account</Link>
   </main>;
 }
